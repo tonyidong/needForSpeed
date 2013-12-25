963ea736afcdb36ffc8a9aeabbdc964be8df9e31
TonyDong <yidong@wustl.edu>
2013-12-25 13:10:12 -0600

    Car updated

--- Car.java
+++ Car.java
@@ -1,6 +1,31 @@
 package needForSpeed;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Car {
+	
+	final int cRow, cCol;
+	final private List<Cell> segments;
+	final private CellManaging cm;
+	
+	public Car(int row, int col, CellManaging cm){
+		this.cm = cm;
+		this.cRow = row;
+		this.cCol = col;
+		this.segments = new LinkedList<Cell>();
+		for(int r = row-1; r <= row+2; r++){
+			for(int c = col-1; c <= col+1; c++){
+				if(c == col || (r == row+1 && c == col-1) || (r == row+1 && c == col+1) || (r == row-1 && c == col-1) || (r == row-1 && c == col+1)){
+					segments.add(cm.allocateCell(r, c));
+				}
+			}
+		}
+	}
+	
+	public void Move(Move move){
+		
+	}
 	
 
 }
--- Self.java
+++ Self.java
@@ -2,4 +2,10 @@ package needForSpeed;
 
 public class Self extends Car{
 
+	public Self(int row, int col, CellManaging cm) {
+		super(row, col, cm);
+		// FIXME Auto-generated constructor stub
+	}
+	
+
 }
